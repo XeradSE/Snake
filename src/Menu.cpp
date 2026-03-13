7eafa749a3db81837bd71c731417cd4ec964b52d
Kylian Betuel <betuelkylian17@gmail.com>
2026-03-13 14:19:26 +0100

    Fixed the texts on the Menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -3,30 +3,31 @@
 //
 
 #include "./../include/Menu.hpp"
-#include "raylib.h"
 #include "./../include/ScoreManager.hpp"
+#include "raylib.h"
 
 void Menu::drawMainMenu() {
-    int x = MeasureText("Snake", 100);
-    DrawText("Snake", 500 - x, 100, 100, DARKGREEN);        
-    x = MeasureText("Appuie sur Entrée pour jouer", 25);
-    DrawText("Appuie sur Entrée pour jouer", 600 - x, 300, 25, DARKGREEN);
+  int x = MeasureText("Snake", 100);
+  DrawText("Snake", 450 - x, 100, 100, DARKGREEN);
+  x = MeasureText("Appuie sur Entrée pour jouer", 25);
+  DrawText("Appuie sur Entrée pour jouer", 500 - x, 300, 25, DARKGREEN);
 }
 
 void Menu::drawGameOver(int score) {
-    int x = MeasureText("GAME OVER", 90);
-    DrawText("GAME OVER", 600 - x, 100, 90, DARKGREEN);
-    x = MeasureText("Score :", 50);
-    DrawText(TextFormat("Score : %i", score), 300 - x, 300, 50, DARKGREEN);
-    x = MeasureText("High Score : ", 50);
-    DrawText(TextFormat("High Score : %i", ScoreManager::loadHighScore()), 400 - x, 350, 50, DARKGREEN);
-    x = MeasureText("Appuie sur Entrée pour rejouer", 25);
-    DrawText("Appuie sur Entrée pour rejouer", 600 - x, 400, 25, DARKGREEN);
+  int x = MeasureText("GAME OVER", 90);
+  DrawText("GAME OVER", 575 - x, 100, 90, DARKGREEN);
+  x = MeasureText("Score :", 50);
+  DrawText(TextFormat("Score : %i", score), 375 - x, 300, 50, DARKGREEN);
+  x = MeasureText("High Score : ", 50);
+  DrawText(TextFormat("High Score : %i", ScoreManager::loadHighScore()),
+           450 - x, 350, 50, DARKGREEN);
+  x = MeasureText("Appuie sur Entrée pour rejouer", 25);
+  DrawText("Appuie sur Entrée pour rejouer", 500 - x, 400, 25, DARKGREEN);
 }
 
 bool Menu::handleInput() {
-    if (IsKeyPressed(KEY_ENTER)) {
-        return true;
-    }
-    return false;
+  if (IsKeyPressed(KEY_ENTER)) {
+    return true;
+  }
+  return false;
 }
